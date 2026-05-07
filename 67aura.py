4c80dadb3c7cc46de7c3802efb9d5a9999c2b569
OED-YZPRO <superyeechong@gmail.com>
2026-05-07 12:10:06 +0800

    Adding shop

--- 67aura.py
+++ 67aura.py
@@ -13,30 +13,46 @@ class user:
     def __init__(self, name):
          self.name = name
          self.luck = 1
+         self.money = 0
+         self.potions = 0
 
 player1 = user(name)
+
+aas = ['uncommon', 'rare', 'epic', 'legendary', 'mythical']
 
 auras = []
 def roll():
     print("You rolled an aura")
     chance = random.randint(1, 100)
     if chance <= 50:
-        print("You got an uncommon aura.")
-        auras.append("Uncommon")
-    elif chance in range(51, 81):
-        print("You got a rare aura")
-        auras.append("Rare")
+        print(f"You got an {aas[0]} aura.")
+        auras.append(aas[0])
+    elif chance in range(51, 81): 
+        print(f"You got an {aas[1]} aura.")
+        auras.append(aas[1])
     elif chance in range(81, 91):
-        print("You got an epic aura")
-        auras.append("Epic")
+        print(f"You got an {aas[2]} aura.")
+        auras.append(aas[2])
     elif chance in range(91, 95):
-        print("You got a legendary aura")
-        auras.append("Legendary")
+       print(f"You got an {aas[3]} aura.")
+        auras.append(aas[3])
     elif chance == 100:
-        print("You got a Mythical aura")
-        auras.append("Mythical")
+      print(f"You got an {aas[4]} aura.")
+        auras.append(aas[4])
     else:
         print("You got nothing lol")
+        
+def shop():
+    while do /= '3':
+        do = input('What do you want to do? \n buy potions - 1 \n sell auras - 2 \n exit the shop - 3 \n')
+        if do == "1":
+            if player1.money >= 50:
+                player1.money -= 50
+            else:
+                print("You don't have enough money")
+        
+    
+    
 
 
 def inventory():
@@ -47,13 +63,25 @@ def main():
     print(f"User's Stats \n Name: {player1.name} \n Luck: {player1.luck} \n")
 
     while start == "1":
-        inp = input("\nActions: \n Roll an aura - 1 \n Check inventory - 2 \n Exit - 3 \n")
+        inp = input("\nActions: \n Roll an aura - 1                                                 \n Check inventory - 2 \n Check stats - 3 \n Shop - 4 \n 6 - drink a potion \n Exit - 5 \n")
         if inp == "1":
             roll()
         elif inp == "2":
             inventory()
+        elif inp == "3":
+            print(f"User's Stats \n Name: {player1.name} \n Luck: {player1.luck} \n")
+        elif inp == "4":
+            start = 0
+            shop()
         else:
             exit()
             
 
 main()
+
+
+
+
+
+
+
